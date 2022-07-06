15ac20440091bd52bb59f2e29d90070c8c2080b6
Fulvio Stefanine <62194735+fustsiju@users.noreply.github.com>
2022-07-05 21:58:53 -0300

    Add files via upload

---
+++ Matriz.js
@@ -0,0 +1,4 @@
+const students = [['Fulvio', 21, 'MG'], ['João', 20, 'MG']['Arthur', 22, 'MG']]
+                   //0       0     0    //1     1     1   //2       2     2
+console.log(students[0][1]) //Receber idade da primeira posição
+console.log(students[2][0]) //Receber nome da terceira posição
